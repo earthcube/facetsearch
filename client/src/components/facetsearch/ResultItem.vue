42800b4a556d5d1ed287ea454afd3dd7308473ee
Bing Zhang <bing@illinois.edu>
2021-11-08 15:12:07 -0600

    collection page

--- client/src/components/facetsearch/ResultItem.vue
+++ client/src/components/facetsearch/ResultItem.vue
@@ -1,41 +1,44 @@
 <template>
-    <b-card tag="article" class="rounded-0"
-        v-on:click="showDetails"
-        v-bind:class="['type_' + item.resourceType.toLowerCase()]"
-    >
-      <router-link  :to="linkTo()">
-        <b-card-title class="name" v-html="item.name">
+  <b-card tag="article" class="rounded-0"
+          v-on:click="showDetails"
+          v-bind:class="['type_' + item.resourceType.toLowerCase()]"
+  >
+    <router-link  :to="linkTo()">
+      <b-card-title class="name" v-html="item.name">
 
-        </b-card-title>
-      </router-link>
-      <b-card-title class="publisher" v-if="item.pubname" v-html="item.pubname"></b-card-title>
+      </b-card-title>
+    </router-link>
+    <b-card-title class="publisher" v-if="item.pubname" v-html="item.pubname"></b-card-title>
 
-        <b-card-text class="description small mb-2" v-if="item.description" v-html="item.description"></b-card-text>
+    <b-card-text class="description small mb-2" v-if="item.description" v-html="item.description"></b-card-text>
 
-        <div class="keywords" v-if="item.kw">
-            <div class="label">Keywords</div>
-            <div class="values "  >
-              <span class="keyword mx-2 text-secondary" v-for="kw in highlightKw(filters, item.kw)" v-bind:key="kw" v-html="kw"></span>
+    <div class="keywords" v-if="item.kw">
+      <div class="label">Keywords</div>
+      <div class="values "  >
+        <span class="keyword mx-2 text-secondary" v-for="kw in highlightKw(filters, item.kw)" v-bind:key="kw" v-html="kw"></span>
 
-            </div>
-        </div>
+      </div>
+    </div>
 
-        <div class="badges mt-2">
-            <b-badge variant="data" class="mr-1"><b-icon class="mr-1" icon="server"></b-icon>{{item.resourceType}}</b-badge>
-            <b-badge variant="tool" class="mr-1" v-if="connectedTools"><b-icon class="mr-1" icon="tools"></b-icon>Connected Tools</b-badge>
-          <span v-if="item.disurl"> <!-- array created in state.js/flatten... -->
+    <div class="badges mt-2">
+      <b-badge variant="data" class="mr-1"><b-icon class="mr-1" icon="server"></b-icon>{{item.resourceType}}</b-badge>
+      <b-badge variant="tool" class="mr-1" v-if="connectedTools"><b-icon class="mr-1" icon="tools"></b-icon>Connected Tools</b-badge>
+      <span v-if="item.disurl"> <!-- array created in state.js/flatten... -->
             <b-badge variant="light" class="mr-1" :href="i"  v-for="i in item.disurl" v-bind:key="i">
 
                 <a class="card-link" target="_blank" v-if="i.length >0" >{{ i}}</a>
             </b-badge>
           </span>
-        </div>
-    </b-card>
+      <b-button variant="link" size="sm" class="ml2-auto" v-on:click="addToCollection">add collection</b-button>
+    </div>
+  </b-card>
 </template>
 
 <script>
+// import Vue from 'vue'
 import _ from 'lodash'
-import {mapActions,mapGetters} from "vuex";
+import {mapActions, mapGetters, mapState} from "vuex";
+import localforage from 'localforage';
 
 export default {
   name: "ResultItem",
@@ -43,20 +46,54 @@ export default {
   data () {
     return {
       filters : this.state.filters,
-      connectedTools: false
-
+      connectedTools: false,
+      clickToAddCollection: false,
     }
   }, computed: {
     ...mapGetters ([
-                  'getConnectedTool'])
-}
+      'getConnectedTool']),
+    ...mapState ([ 'collections'])
+  }
   ,mounted() {
-   this.hasTool();
+    this.hasTool();
 
   }
   , methods: {
     ...mapActions([
-       'hasConnectedTools']),
+      'hasConnectedTools']),
+    addToCollection() {
+      var self = this
+      this.clickToAddCollection = true
+      // var toAdd = true
+      // for(var i = 0; i < this.collections.length; i++) {
+      //   var item = this.collections[i]
+      //   if (item.g === this.item.g) {
+      //     toAdd = false
+      //     break
+      //   }
+      // }
+      localforage.getItem(this.item.g, function (err, value) {
+        if (value === null) {
+          localforage.setItem(
+              self.item.g,
+              self.item
+          ).then((value) => {
+            console.log("store " + value.g + " to localstorage");
+          }).catch((err) => {
+            console.log('oops! the account was too far gone, there was nothing we could do to save him ', err);
+          });
+          console.log("add to collection");
+        } else {
+          // localforage.setItem(newFilename, value, function () {
+          //   localforage.removeItem(filename, function () { return callback(); });
+          // });
+          console.log(value)
+        }
+      });
+      // if(toAdd) {
+      //   // Vue.set(this.collections, this.collections.length, this.item)
+      // }
+    },
     linkTo() {
       if (this.item.resourceType === 'tool') {
         return {
@@ -77,7 +114,11 @@ export default {
       }
     },
     showDetails() {
-
+      if(this.clickToAddCollection) {
+        this.clickToAddCollection = false;
+        //
+        return
+      }
       if (this.item.resourceType==='tool'){
         this.$router.push({
           name: 'tool',
@@ -131,7 +172,7 @@ export default {
         self.hasConnectedTools(gg).then(
             function(o){
 
-                self.connectedTools=o;
+              self.connectedTools=o;
 
             }
         ).catch((err) => {
@@ -142,7 +183,7 @@ export default {
     }
   },
   makeToast(mesg="Error") {
-   let message = `Unknown type. Send us this identifier ${mesg}`
+    let message = `Unknown type. Send us this identifier ${mesg}`
     this.$bvToast.toast(message, {
       title: 'Cannot locate item',
       autoHideDelay: 5000,
@@ -154,102 +195,102 @@ export default {
 </script>
 
 <style scoped lang="scss">
-    @import '~/src/assets/bootstrapcss/custom';
+@import '~/src/assets/bootstrapcss/custom';
 
 article {
-    cursor: pointer;
+  cursor: pointer;
 
-    border: {
-        right: 0px;
-        left: 0px;
-    }
+  border: {
+    right: 0px;
+    left: 0px;
+  }
 
-    &:hover {
-        background: {
-            color: $gray-300;
-        }
+  &:hover {
+    background: {
+      color: $gray-300;
     }
+  }
 
-    //special css treatment for data or tool results
-/*
-    &.type_data {
-        &:not(:hover) {
-            background: {
-                color: rgba($data, .1);
-            }
-        }
-    }
+  //special css treatment for data or tool results
+  /*
+      &.type_data {
+          &:not(:hover) {
+              background: {
+                  color: rgba($data, .1);
+              }
+          }
+      }
 
-    &.type_tool {
-        &:not(:hover) {
-            background: {
-                color: rgba($tool, .1);
-            }
-        }
-    }
-*/
-    .card-body {
-        padding: ($spacer * 1.5) $spacer;
-    }
+      &.type_tool {
+          &:not(:hover) {
+              background: {
+                  color: rgba($tool, .1);
+              }
+          }
+      }
+  */
+  .card-body {
+    padding: ($spacer * 1.5) $spacer;
+  }
 }
 
 .keywords {
-    display: flex;
+  display: flex;
 
+  font: {
+    size: 80%;
+  }
+
+  .label {
     font: {
-        size: 80%;
+      weight: bold;
     }
-
-    .label {
-        font: {
-            weight: bold;
-        }
-        text: {
-            transform: uppercase;
-        }
+    text: {
+      transform: uppercase;
     }
+  }
 
-    .values {
-      display: flex;
-      white-space:nowrap;
-      flex-wrap: wrap;
+  .values {
+    display: flex;
+    white-space:nowrap;
+    flex-wrap: wrap;
 
-        .keyword {
+    .keyword {
 
-            padding: {
-                left: $spacer / 2;
-            }
-        }
+      padding: {
+        left: $spacer / 2;
+      }
     }
+  }
 }
 
 .name {
-    color: $gray-800;
+  color: $gray-800;
 
-    font: {
-        weight: 600;
-        size: 120%;
-    }
-    line: {
-        height: 120%;
-    }
+  font: {
+    weight: 600;
+    size: 120%;
+  }
+  line: {
+    height: 120%;
+  }
 }
 
 .publisher {
-    color: $gray-500;
+  color: $gray-500;
 
-    margin: {
-        top: -($spacer * .4);
-    }
+  margin: {
+    top: -($spacer * .4);
+  }
 
-    font: {
-        style: italic;
-        size: 90%;
-    }
+  font: {
+    style: italic;
+    size: 90%;
+  }
 }
 
 .description {
-    color: $gray-500;
+  color: $gray-500;
 }
 
 </style>
