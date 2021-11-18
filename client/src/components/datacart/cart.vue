612acc6a119049f9f1aab12962114521f01eaed4
David Valentine <david.valentine@gmail.com>
2021-11-18 11:08:14 -0800

    reconfigure notebook proxy on staging&development configs to beta container

---
+++ client/src/components/datacart/cart.vue
@@ -0,0 +1,31 @@
+<template>
+  <b-container fluid="md">
+    <b-row>
+      <b-col md="3" class="sidebar">
+
+      </b-col>
+      <b-col md="4">
+ <list>
+
+ </list>
+      </b-col >
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+// https://matthiashager.com/complete-vuejs-application-tutorial/localstorage-vuejs#aeb433212d1
+
+import list from './list'
+export default {
+  name: "cart",
+  components: {
+    list,
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/datacart/list.vue
@@ -0,0 +1,17 @@
+<template>
+<div >
+  <div>Item</div>
+  <div>Item</div>
+  <div>Item</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "list"
+}
+</script>
+
+<style scoped>
+
+</style>
