c013dffd405aad115178be62a67cd13b60521425
David Valentine <david.valentine@gmail.com>
2022-01-18 15:53:51 -0800

    webppack

--- client/vue.config.js
+++ client/vue.config.js
@@ -27,14 +27,19 @@ module.exports = {
                 maxEntrypointSize: 1048576, // int (in bytes)
             },
             optimization: {
+                runtimeChunk: 'single',
                 splitChunks: {
+                    chunks: "all",
+                    maxInitialRequests: Infinity,
+                    minSize: 0,
                     cacheGroups: {
-                        shared: {
+                        vendor: {
                             test: /[\\/]node_modules[\\/]/,
-                            name: 'vendor',
-                            enforce: true,
-                            chunks: 'all',
-                        }
+                            name(module) {
+                                const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                                return `npm.${packageName.replace('@', '')}`;
+                            },
+                        },
                     }
                 }
             }
