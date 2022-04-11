446bbcc2a24046710fe7372ac066cbca60a72ea5
Bing Zhang <bing@illinois.edu>
2022-04-11 14:15:24 -0500

    updates GUI

--- client/src/components/facetsearch/Collection.vue
+++ client/src/components/facetsearch/Collection.vue
@@ -111,10 +111,10 @@
                           </div>
 
                         </b-col>
-                        <b-col>
-                          <!--                        <b-button variant="link" size="sm" class="ml2-auto" v-on:click="moveToCollection(item)">Move to Collections</b-button>-->
-                          <b-button variant="outline-primary" size="sm" class="ml2-auto" v-on:click="moveToCollection(item)">Move</b-button>
-                        </b-col>
+<!--                        <b-col>-->
+<!--                          &lt;!&ndash;                        <b-button variant="link" size="sm" class="ml2-auto" v-on:click="moveToCollection(item)">Move to Collections</b-button>&ndash;&gt;-->
+<!--                          <b-button variant="outline-primary" size="sm" class="ml2-auto" v-on:click="moveToCollection(item)">Move</b-button>-->
+<!--                        </b-col>-->
                       </b-row>
                     </b-container>
                   </div>
@@ -135,8 +135,8 @@
                                     multiple
                                     v-model="item.collections"
                                     :id="'accordion_text_'+ item.value.name"
-                                    :options="item.collections"
-                                    @input="(name) => updateRemovedCollection(item, name)"
+                                    :options="facetSetting.names"
+                                    @input="(name) => updateRemovedCollection(item, name, false)"
                                 ></v-select>
                               </div>
                             </div>
@@ -152,7 +152,7 @@
                     <div v-if="$data.currentClick =='unassigned'">
                       <b-container fluid="md" class="mt-3">
                         <b-row>
-                          Can be removed from collections:
+                          In Collections:
                         </b-row>
 <!--                        <b-row>-->
 <!--                          <div v-for="removecollection in item.removeCollection" v-bind:key="removecollection">-->
@@ -171,16 +171,17 @@
                                           multiple
                                           v-model="item.assignedCollections"
                                           :id="'accordion_text_'+ item.value.name"
-                                          :options="item.collections"
-                                          @input="(name) => updateRemovedCollection(item, name)"
+                                          :options="facetSetting.names"
+                                          v-on:option:selected="updateRemovedCollection(item, $event, false)"
+                                          v-on:option:deselected="optionRemoved($event, item)"
                                 ></v-select>
                               </div>
                             </div>
                           </b-col>
-                          <b-col>
-                            <b-button variant="outline-primary" size="sm" class="ml2-auto" v-on:click="removeFromCollection(item)">Remove</b-button>
-                            <confirm-dialogue ref="confirmDialogue"></confirm-dialogue>
-                          </b-col>
+<!--                          <b-col>-->
+<!--                            <b-button variant="outline-primary" size="sm" class="ml2-auto" v-on:click="removeFromCollection(item)">Remove</b-button>-->
+<!--                            <confirm-dialogue ref="confirmDialogue"></confirm-dialogue>-->
+<!--                          </b-col>-->
                         </b-row>
                       </b-container>
                     </div>
@@ -219,7 +220,7 @@ import {mapGetters} from "vuex";
 import vSelect from "vue-select";
 import VueScrollbox from 'vue-scrollbox';
 import "vue-select/dist/vue-select.css";
-import ConfirmDialogue from './ConfirmDialogue.vue'
+// import ConfirmDialogue from './ConfirmDialogue.vue'
 
 Vue.component("v-select", vSelect);
 Vue.component('vue-scrollbox', VueScrollbox);
@@ -230,7 +231,7 @@ export default {
     // FacetTextItem,
     CreateCollection,
     CollectionMenuItem,
-    ConfirmDialogue,
+    // ConfirmDialogue,
   },
   data () {
     return {
@@ -363,13 +364,59 @@ export default {
         console.log(err);
       });
     },
-    updateRemovedCollection:  function(item, collectionNames) {
-      item.removeCollection = collectionNames
+    optionRemoved: function(option, item) {
+      console.log(option + ", " + item)
+      this.updateRemovedCollection(item, [option], true)
+    },
+    updateRemovedCollection:  function(item, collectionNames, isRemove) {
+      if (collectionNames.length === 0) return
+      var addedCollections = collectionNames
+      if('removeCollection' in item && item.removeCollection.length > 0)
+        addedCollections = collectionNames.filter(collname => (!item.removeCollection.includes(collname)))
+      for(let i = 0; i < collectionNames.length; i++) {
+        if(!item.removeCollection.includes(collectionNames[i])) {
+          item.removeCollection.push(collectionNames[i])
+        }
+        if(isRemove && 'assignedCollections' in item)
+          if(item.assignedCollections.includes(collectionNames[i])) {
+            item.assignedCollections.remove(collectionNames[i])
+          }
+      }
+      // item.removeCollection = collectionNames
+      if (addedCollections.length > 0) {
+        console.log("updateRemovedCollection: add to coll: " + addedCollections)
+        this.moveToCollection(item)
+      } else {
+        // var removedCollections = item.removeCollection.filter(collname => (!collectionNames.includes(collname)))
+        console.log("updateRemovedCollection: remove from coll: " + collectionNames)
+        this.removeFromCollection(item)
+      }
     },
     updateMovedToCollection:  function(item, collectionNames) {
+      if (collectionNames.length === 0) return
       console.log(item.collection)
       console.log(collectionNames)
+      var addedCollections = collectionNames
+      if('assignedCollections' in item) {
+        for (let i = 0; i < collectionNames.length; i++) {
+          if (!item.assignedCollections.includes(collectionNames[i])) {
+            item.assignedCollections.push(collectionNames[i])
+          }
+        }
+      } else {
+        item.assignedCollections = collectionNames
+      }
+      if('moveCollection' in item && item.moveCollection.length > 0)
+        addedCollections = collectionNames.filter(collname => (!item.moveCollection.includes(collname)))
       item.moveCollection = collectionNames
+      if (addedCollections.length > 0) {
+        console.log("updateMovedToCollection: add to coll: " + addedCollections)
+        this.moveToCollection(item)
+      } else {
+        var removedCollections = item.moveCollection.filter(collname => (!collectionNames.includes(collname)))
+        console.log("updateMovedToCollection: remove from coll: " + removedCollections)
+        this.removeFromCollection(item)
+      }
     },
     async removeFromCollection(item) {
       if (item.removeCollection.length === 0)
@@ -377,14 +424,15 @@ export default {
       var self = this
       console.log("removeFromCollection")
 
-      const ok = await this.$refs.confirmDialogue[0].show({
-        title: 'Remove Confirmation',
-        message: 'Are you sure you want to remove this item from ' + item.assignedCollections + ' collection(s)?',
-        okButton: 'Remove',
-      })
+      // const ok = await this.$refs.confirmDialogue[0].show({
+      //   title: 'Remove Confirmation',
+      //   message: 'Are you sure you want to remove this item from ' + item.assignedCollections + ' collection(s)?',
+      //   okButton: 'Remove',
+      // })
+      const ok = true
       if (ok) {
         // var newColl = item.collections.filter(collname => (!item.removeCollection.includes(collname)))
-        item.assignedCollections = [...item.collections.filter(collname => (!item.assignedCollections.includes(collname)))]
+        // item.assignedCollections = [...item.collections.filter(collname => (!item.assignedCollections.includes(collname)))]
         // item.collections = newColl
         // write back to storage.
         localforage.getItem(item.value.g, function (err, value) {
@@ -422,7 +470,7 @@ export default {
             // Vue.set(self.collections, item.type, currentColl)
             console.log("reload collection after remove from collection")
             Vue.set(self.types, item.type, {'name': item.type, 'content': content})
-            self.reloadCollections()
+            // self.reloadCollections()
             self.chooseType("unassigned", "")
             // if (item.assignedCollections.length === 0) {
             //   self.chooseType("unassigned", "")
@@ -443,7 +491,7 @@ export default {
       console.log("moveToCollection")
       console.log(item)
       var self = this
-      item.collections = [...item.moveCollection]
+      item.collections = [...item.assignedCollections]
       item.removeCollection = [...item.collections]
       // write back to storage.
       localforage.getItem(item.value.g, function (err, value) {
@@ -467,7 +515,7 @@ export default {
           // Vue.set(self.collections, item.type, currentColl)
           Vue.set(self.types, item.type, {'name': item.type, 'content': content})
           console.log("reload collection after move to collections");
-          self.reloadCollections()
+          // self.reloadCollections()
           self.chooseType("unassigned", "")
         }).catch((err) => {
           console.log('oops! the account was too far gone, there was nothing we could do to save him ', err);
