66900d04806f9ff6f388c3ac5faaf677945811f4
David Valentine <david.valentine@gmail.com>
2022-04-13 12:56:54 -0700

    config to use prod servers

--- server/config/production.js
+++ server/config/production.js
@@ -1,12 +1,14 @@
 var endPoint = process.env.S3ADDRESS || "oss.geodex.org"
 var accessKey = process.env.S3KEY || 'AKIAIOSFODNN7EXAMPLE'
 var secretKey = process.env.S3SECRET || 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+var useSSL = process.env.S3SSL || true
+var s3Port =  process.env.S3PORT || 443
 exports.config = {
     jsonldStore: {
         "type": "minio",
         "endPoint": endPoint,
-        "port": 443,
-        useSSL: true,
+        "port": s3Port,
+        useSSL: useSSL,
         accessKey: accessKey,
         secretKey: secretKey
 
