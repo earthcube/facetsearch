8ca8a78b6ca7f17e6b0fb527b5de4d3022d53e8e
David Valentine <david.valentine@gmail.com>
2022-08-12 11:06:11 -0700

    try to embedd yasgui

--- client/src/components/spaqlgui/sparqlgui.vue
+++ client/src/components/spaqlgui/sparqlgui.vue
@@ -22,7 +22,54 @@ export default {
   },
   async mounted( ){
     let s = this.$refs.sparqlyasgui
-    this.yasgui = new Yasgui(s);
+
+    this.yasgui = new Yasgui(s,
+        {
+          tabName: "EC Query",
+          requestConfig: {endpoint: "https://graph.geodex.org/blazegraph/namespace/earthcube/sparql"},
+          // Allow resizing of the Yasqe editor
+          resizeable: true,
+
+          // Whether to autofocus on Yasqe on page load
+          autofocus: true,
+
+          copyEndpointOnNewTab: true,
+          endpointCatalogueOptions: {
+            getData: () => {
+              return [
+                //List of objects should contain the endpoint field
+                //Feel free to include any other fields (e.g. a description or icon
+                //that you'd like to use when rendering)
+                {
+                  endpoint: "https://graph.geodex.org/blazegraph/namespace/earthcube/sparql"
+                },
+                {
+                  endpoint: "https://graph.geocodes.earthcube.org/blazegraph/namespace/earthcube/sparql"
+                },
+                {
+                  endpoint: "https://graph.geocodes-dev.earthcube.org/blazegraph/namespace/earthcube/sparql"
+                }
+                // ...
+              ];
+            },
+            keys: [],
+            renderItem: (data, source) => {
+              const contentDiv = document.createElement("div");
+
+              contentDiv.style.display = "flex";
+              contentDiv.style.flexDirection = "column";
+              const endpointSpan = document.createElement("span");
+              endpointSpan.innerHTML =
+                  data.matches.endpoint?.reduce(
+                      (current, object) => (object.highlight ? current + object.text.bold() : current + object.text),
+                      ""
+                  ) || "";
+              contentDiv.appendChild(endpointSpan);
+              source.appendChild(contentDiv);
+            },
+          }
+        }
+        ) ;
   }
 
 }
