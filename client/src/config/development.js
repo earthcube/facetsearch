1ff512f9f26ba08b5adf6abcabf122eb163274e1
m bobak <mbobak@illinois.edu>
2022-10-03 14:47:05 -0500

    to try in dev w/dev endpoint

--- client/src/config/development.js
+++ client/src/config/development.js
@@ -1,7 +1,9 @@
 export default {
     NODE_ENV: process.env.NODE_ENV || 'unknown',
     API_URL: 'http://localhost:3000',
-    TRIPLESTORE_URL:'https://graph.geocodes.earthcube.org/blazegraph/namespace/earthcube/sparql',
+    //TRIPLESTORE_URL:'https://graph.geocodes.earthcube.org/blazegraph/namespace/earthcube/sparql',
+    //TRIPLESTORE_URL:'http://mbobak.ncsa.illinois.edu:9999/blazegraph/namespace/nabu/sparql',
+    TRIPLESTORE_URL:'https://graph.geocodes-dev.earthcube.org/blazegraph/namespace/https/sparql',
     //TRIPLESTORE_URL:'https://graphdb.geodex.org/repositories/geocodes',
     ECRR_TRIPLESTORE_URL:'https://graph.geocodes.earthcube.org/blazegraph/namespace/ecrr/sparql',// dev
     //ECRR_TRIPLESTORE_URL:'http://132.249.238.169:8080/fuseki/ecrr/query', // fuseki
