627503fefb5a442b72256594e4f098cce9e4985c
David Valentine <david.valentine@gmail.com>
2022-10-20 11:23:48 -0700

    mustash bucket templates so they can be passed as env variables

--- server/config/production.js
+++ server/config/production.js
@@ -6,10 +6,10 @@ var s3Port =  process.env.S3PORT || 443
 var bucket = process.env.BUCKET || 'gleaner'
 var bucketpath = process.env.BUCKETPATH || 'summoned'
 // options for path: bucket, bucketpath, reponame, sha
-var pathtemplate = process.env.PATHTEMPLATE || '${bucketpath}/${reponame}/${sha}.jsonld'
+var pathtemplate = process.env.PATHTEMPLATE || '{{bucketpath}}/{{reponame}}/{{sha}}.jsonld'
 
 // options for tool: bucket, bucketpath, reponame, ref (aka ark, or maybe a sha in the future)
-var tooltemplate = process.env.TOOLTEMPLATE || '${bucketpath}/${reponame}/${ref}.json'
+var tooltemplate = process.env.TOOLTEMPLATE || '{{bucketpath}}/{{reponame}}/{{ref}}.json'
 var toolbucket = process.env.TOOLBUCKET || 'ecrr'
 var toolpath = process.env.TOOLPATH || 'summoned'
 var toolrepo = process.env.TOOLREPO ||'ecrr_form'
