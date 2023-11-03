3fee978ab5a23714de986e8f23412bc69cf37cd0
Ya-Lan Yang <ylyang@illinois.edu>
2023-11-02 21:03:07 -0500

    fixed tables

--- client/src/components/help/report.vue
+++ client/src/components/help/report.vue
@@ -1,9 +1,8 @@
 <template>
     <b-container fluid="md" class="mt-5">
         <div>
-            <h2>{{ this.source}}</h2>
+            <b-card header="Missing Report">
           <div  v-if="missing">
-              <h5>Missing Report</h5>
               <a :href="missing_url">Download Full Report</a>
               <p>Report Date: {{ missing.date }}</p>
               <ul>
@@ -13,9 +12,11 @@
                   <li>Graph URN count: {{ missing.graph_urn_count }}</li>
                   <li>Missing graph URN count: {{ missing.missing_summon_graph_count }}</li>
               </ul>
+
             </div>
+            </b-card>
           <div  v-if="graphinfo">
-              <h5>Graph Stats</h5>
+              <b-card header="Graph Stats">
               <a :href="graph_url">Download Full Report</a>
               <p>Report Date: {{ graphinfo.date }}</p>
               <ul>
@@ -23,38 +24,62 @@
                 <b-list-group-item
                   v-for="(item, index) in graphinfo.reports"
                   :key="index"
+                  class = "customList"
                 >
-                    <li v-if="item.report=='dataset_count'">{{ item.report }} :
-                        <b-list-group>
-                            <b-list-group-item
+                    <li v-if="item.report=='dataset_count'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.datasetcount }}
+                        </p>
+                    </li>
+                    <li v-if="item.report=='triple_count'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.tripelcount }}
+                        </p>
+                    </li>
+                    <li v-if="item.report=='repos_with_keywords'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.kwcount }}
+                        </p>
+                    </li>
+                    <li v-if="item.report=='graph_count_by_repo'">{{ item.report }} :
+                        <table class="customTable">
+                            <thead>
+                            <tr>
+                                <th>graphs</th>
+                                <th>triples</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
                               v-for="(report, report_index) in item.data"
                               :key="report_index"
                             >
-                                {{ report.datasetcount }}
-                            </b-list-group-item>
-                        </b-list-group>
+                                <td>{{ report.graphs }}</td>
+                                <td>{{ report.triples }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </li>
-                    <li v-if="item.report=='triple_count'">{{ item.report }} :
-                        <b-list-group>
-                            <b-list-group-item
+                    <li v-if="item.report=='dataset_count_by_repo'">{{ item.report }} :
+                        <table class="customTable">
+                            <thead>
+                            <tr>
+                                <th>graphs</th>
+                                <th>datasets</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
                               v-for="(report, report_index) in item.data"
                               :key="report_index"
                             >
-                                {{ report.tripelcount }}
-                            </b-list-group-item>
-                        </b-list-group>
+                                <td>{{ report.graphs }}</td>
+                                <td>{{ report.datasets }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
                     </li>
-                    <li v-if="item.report=='kw_count'">{{ item.report }} :
-                        <b-list-group>
-                            <b-list-group-item
-                              v-for="(report, report_index) in item.data"
-                              :key="report_index"
-                            >
-                                {{ report.kwcount }}
-                            </b-list-group-item>
-                        </b-list-group>
-                    </li>
-                    <li v-if="item.report=='type_count'">{{ item.report }} :
+                    <li v-if="item.report=='types_count'">{{ item.report }} :
                         <table class="customTable">
                             <thead>
                             <tr>
@@ -74,31 +99,32 @@
                         </table>
                     </li>
                     <li v-if="item.report=='version_count'">{{ item.report }} :
-                        <b-list-group>
-                            <b-list-group-item
+                    <li v-if="item.report=='version_count'">{{ item.report }} :
+                        <v-list>
+                            <v-list-item
                               v-for="(report, report_index) in item.data"
                               :key="report_index"
                             >
                                 {{ report.versioncount }}
-                            </b-list-group-item>
-                        </b-list-group>
+                            </v-list-item>
+                        </v-list>
                     </li>
                     <li v-if="item.report=='variablename_count'">{{ item.report }} :
-                        <b-list-group>
-                            <b-list-group-item
+                        <v-list>
+                            <v-list-item
                               v-for="(report, report_index) in item.data"
                               :key="report_index"
                             >
                                 {{ report.variablenamecount }}
-                            </b-list-group-item>
-                        </b-list-group>
+                            </v-list-item>
+                        </v-list>
                     </li>
-                    <li v-if="item.report=='graph_sizes_count'">{{ item.report }} :
+                    <li v-if="item.report=='types_count_by_repo'">{{ item.report }} :
                         <table class="customTable">
                             <thead>
                             <tr>
-                                <th>triple_per_jsonld</th>
-                                <th>count</th>
+                                <th>type</th>
+                                <th>type_counts</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -106,35 +132,27 @@
                               v-for="(report, report_index) in item.data"
                               :key="report_index"
                             >
-                                <td>{{ report.triple_per_jsonld }}</td>
-                                <td>{{ report.count }}</td>
+                                <td>{{ report.type }}</td>
+                                <td>{{ report.type_counts }}</td>
                             </tr>
                             </tbody>
                         </table>
                     </li>
-                    <li v-if="item.report=='triple_count_by_graph'">{{ item.report }} :
-                        <table class="customTable">
-                            <thead>
-                            <tr>
-                                <th>g</th>
-                                <th>count</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr
-                              v-for="(report, report_index) in item.data"
-                              :key="report_index"
-                            >
-                                <td>{{ report.g }}</td>
-                                <td>{{ report.count }}</td>
-                            </tr>
-                            </tbody>
-                        </table>
+                    <li v-if="item.report=='mutilple_version_count_by_repo'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.versioncount }}
+                        </p>
+                    </li>
+                    <li v-if="item.report=='variablename_count'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.variablenamecount }}
+                        </p>
                     </li>
                 </b-list-group-item>
               </b-list-group>
 
               </ul>
+              </b-card>
             </div>
           </div>
     </b-container>
@@ -229,6 +247,15 @@ th {
   text-align: center;
     background: #18598b;
     color: white;
+}
+
+th, td {
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.customList {
+    border: none
 }
 
 </style>
