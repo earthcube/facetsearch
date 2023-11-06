f9e30cb1b236eefce3defae1a9ad168b2750bfaa
Ya-Lan Yang <63822845+ylyangtw@users.noreply.github.com>
2023-11-06 13:46:39 -0600

    109 repository summaries for about page (#122)
    * imported axios
    
    * added all
    
    * fix  logos
    
    * remove hard coded info
    
    * added report dropdown
    
    * Update about.vue
    
    * added report
    
    * Update report.vue
    
    * 117 master fixuri (#119)
    
    * #117 fix for changed uri path
    This is done with an env
     UNIVERSION=v2
    
    * fix about to use a passed in property
    using bootstrap-vue, not vuetify
    reports a little less fixed,
    
    * fix
    
    * Revert "Merge branch '109-repository-summaries-for-about-page' of https://github.com/earthcube/facetsearch into 109-repository-summaries-for-about-page"
    
    This reverts commit e9f5766b02cd0c9b39d715b2887405045286059a, reversing
    changes made to fbb79e7803dc55779812b8b7884708ae2c1a2c88.
    
    * Revert "fix"
    
    This reverts commit fbb79e7803dc55779812b8b7884708ae2c1a2c88.
    
    * fix about to use a passed in property
    using bootstrap-vue, not vuetify
    reports a little less fixed,
    
    * fix
    
    * fixed tables
    
    * Add configurable url for s3 reports.
    Fix table to match the changed report
    
    * Add configurable url for s3 reports.
    Fix table to match the changed report
    Flipped logic to try the json parsing first
    
    ---------
    
    Co-authored-by: David Valentine <david.valentine@gmail.com>

--- client/src/components/help/about.vue
+++ client/src/components/help/about.vue
@@ -36,10 +36,11 @@ in case more intro paragraph text is needed
 
         <b-card-group columns class="mt-4">
             <b-card no-body class="text-center"
-                v-for="(item, index) in repositories"
+                v-for="(item, index) in info"
                 v-bind:key="index"
             >
-                <b-card-body>
+
+                <b-card-body v-if="item.source!='geocodes_demo_datasets'">
                     <b-card-title>
                         <b-link target="_blank" class="d-flex flex-column align-items-center"
                             :href="item.website"
@@ -55,10 +56,16 @@ in case more intro paragraph text is needed
                     </b-card-title>
 
                     <b-card-text>
-                        <i v-if="item.record_count > 0">{{item.record_count}} record{{(item.record_count == 1) ? '' : 's'}}</i>
+                        <i v-if="item.records > 0">{{item.records}} record{{(item.records == 1) ? '' : 's'}}</i>
 
                         <div class="mt-3 small text-left" v-html="item.description"></div>
+
+                        <div class="d-flex justify-content-end" >
+                            <router-link :to="{ name: 'report', params: { source: item.source }}">Reports</router-link>
+                        </div>
+
                     </b-card-text>
+
 <!--
 //left this here in case the description was too much to be shown all the time (use collapse). problem is, sometimes expanding forces an item to move to a different column (feels like it disappears)
 //could use accordian option to only allow a single card to be expanded at a time...but still doesn't solve the issue completely and why this was moved to show the description by default
@@ -79,6 +86,7 @@ in case more intro paragraph text is needed
                         </div>
                     </b-card-text>
 -->
+
                 </b-card-body>
             </b-card>
         </b-card-group>
@@ -97,7 +105,7 @@ in case more intro paragraph text is needed
 
             <p>Work on this site is supported by</p>
 
-            <div><img src="https://www.nsf.gov/images/logos/NSF_4-Color_bitmap_Logo.png" width="38" alt="NSF"></div>
+            <div><img src="https://new.nsf.gov/themes/custom/nsf_theme/components/images/logo/logo-desktop.svg" width="150" alt="NSF"></div>
             <h5>NSF EarthCube</h5>
             <div><i>NSF award #1928208</i></div>
 
@@ -110,134 +118,33 @@ in case more intro paragraph text is needed
 </template>
 
 <script>
+
+import axios from "axios";
+import $ from "jquery";
+
 export default {
   name: "about.vue",
   data() {
     return {
-        repositories: [
-            {
-                title: 'Environmental Data Initiative',
-                website: 'https://edirepository.org/',
-                image: 'edi.png',
-                description: "The Environmental Data Initiative is an NSF-funded project meant to accelerate curation and  archive of environmental data, emphasizing data from projects funded by the NSF Division of Environmental Biology. Aside from hosting the LTER data repository and collaborating with the LTER Information Managers, EDI serves the ecological research community including but not limited to research funded by the Long TermResearch in Environmental Biology (LTREB), Organization for Biological Field Stations (OBFS), and Macrosystems Biology (MSB). The Environmental Data Initiative maintains and develops the PASTA infrastructure for its data repositories, and has recently become DataONE’s 40th member node.",
-                record_count: 28248
-            },
-            {
-                title: 'Biological and Chemical Oceanography Data Management Office',
-                website: 'http://www.bco-dmo.org',
-                image: 'bco-dmo.png',
-                description: "The Biological and Chemical Oceanography Data Management Office (BCO-DMO) was created in late 2006 to serve PIs funded by the NSF Geosciences Directorate (GEO) Division of Ocean Sciences (OCE) Biological and Chemical Oceanography Programs and Office of Polar Programs (OPP) Antarctic Sciences (ANT) Organisms & Ecosystems Program. The BCO-DMO is a combination the Data Management Offices formerly created to support the US JGOFS and US GLOBEC programs. The BCO-DMO staff members are the curators of the legacy data collections created by those respective programs, as well as many other more recent research efforts including those of individual investigators.",
-                record_count: 5018
-            },
-            {
-                title: 'Consortium of Universities for the Advancement of Hydrologic Science, Inc. (CUAHSI)',
-                website: 'http://www.cuahsi.org',
-                image: 'hydroshare.png',
-                description: "CUAHSI's Hydrologic Information System (CUAHSI-HIS) provides web services, tools, standards and procedures that enhance access to more and better data for hydrologic analysis.",
-                record_count: 4185
-            },
-            {
-                title: 'IEDA (Integrated Earth Data Applications)',
-                website: 'http://www.iedadata.org',
-                image: 'iedadata.png',
-                description: "IEDA is a community-based facility that serves to support, sustain, and advance the geosciences by providing data services for observational Geoscience data from the Ocean, Earth, and Polar Sciences. IEDA welcomes and encourages investigators to contribute their data to the IEDA collections so that the data can be discovered and reused by a diverse community now and in the future. The IEDA collections are: EarthChem, Geochron, System for Earth Sample Registration (SESAR), Marine Geoscience Data System (MGDS), and USAP Data Center. Meta-Search provided on the portal through IEDA Data Browser http://www.iedadata.org/databrowser.",
-                record_count: 7147
-            },
-            {
-                title: 'OpenTopography',
-                website: 'http://www.opentopography.org',
-                image: 'opentopography.png',
-                description: "OpenTopography facilitates community access to high-resolution, Earth science-oriented, topography data, and related tools and resources. The OpenTopography Facility is based at the San Diego Supercomputer Center at the University of California, San Diego and is operated in collaboration with colleagues in the School of Earth and Space Exploration at Arizona State University. Core operational support for OpenTopography comes from the National Science Foundation Earth Sciences: Instrumentation and Facilities Program (EAR/IF) and the Office of Cyberinfrastructure. In addition, we receive funding from the NSF and NASA to support various OpenTopography related research and development activities.",
-                record_count: 502
-            },
-            {
-                title: 'UNAVCO',
-                website: 'http://www.unavco.org',
-                image: 'unavco.png',
-                description: "UNAVCO promotes research by providing access to data that our community of geodetic scientists uses for quantifying the motions of rock, ice and water that are monitored by a variety of sensor types at or near the Earth's surface. After processing, these data enable millimeter-scale surface motion detection and monitoring at discrete points, and high-resolution strain imagery over areas of tens of square meters to hundreds of square kilometers. The data types include GPS/GNSS, imaging data such as from SAR and TLS, strain and seismic borehole data, and meteorological data. Most of these can be accessed via web services. In addition, GPS/GNSS datasets, TLS datasets, and InSAR products are assigned digital object identifiers.",
-                record_count: 5086
-            },
-            {
-                title: 'IODP Site Survey Databank',
-                website: 'https://ssdb.iodp.org',
-                image: 'ssdb.iodp.png',
-                description: "The Site Survey Data Bank (SSDB) is a repository for site survey data submitted in support of International Ocean Discovery Program (IODP) proposals and expeditions. SSDB serves different roles for different sets of users.",
-                record_count: 5344
-            },
-            {
-                title: 'Balto',
-                website: 'http://balto.opendap.org',
-                image: 'balto.png',
-                description: "Driven by data-rich use cases that span geodesy, geodynamics, seismology, and ecohydrology, the BALTO project will enable brokered access to diverse geoscience data, including data that have been collected/organized by individual scientists in novel or unusual forms, also known as “long-tail” datasets.",
-                record_count: 89
-            },
-            {
-                title: 'Linked Earth',
-                website: 'http://wiki.linked.earth',
-                image: 'linked.earth.png',
-                description: "The LinkedEarth wiki is an EarthCube-funded platform that: enables the curation of publicly-accessible database by paleoclimate experts, and, fosters the development of standards, so paleoclimate data are easier to analyze, share, and re-use.",
-                record_count: 17964
-            },
-            {
-                title: 'IRIS',
-                website: 'http://iris.edu',
-                image: 'iris.png',
-                description: "Fosters the development of standards, so paleoclimate data are easier to analyze, share, and re-use.",
-                record_count: 28
-            },
-            {
-                title: 'UCAR',
-                website: 'http://data.ucar.edu',
-                image: 'ucar.png',
-                description: "OpenSky is the home for NCAR/UCAR research and historical materials as well as other collections of digital assets. Our mission is to provide free and open access to the scientific output and other intellectual resources created at NCAR/UCAR for the advancement of the atmospheric and related sciences. You can reach us at opensky@ucar.edu with any questions.",
-                record_count: 11672
-            },
-            {
-                title: 'Magnetics Information Consortium (MagIC)',
-                website: 'http://www.earthref.org/MagIC',
-                image: 'magic.png',
-                description: "Magnetics Information Consortium (MagIC) improves research capacity in the Earth and Ocean sciences by maintaining an open community digital data repository for rock and paleomagnetic data with portals that allow users access to archive, search, visualize, download, and combine these versioned datasets.",
-                record_count: 4136
-            },
-            {
-                title: 'Neotoma',
-                website: 'http://www.neotomadb.org',
-                image: 'neotomadb.png',
-                description: "Neotoma is a multiproxy paleoecological database that covers the Pliocene-Quaternary, including modern microfossil samples. The database is an international collaborative effort among individuals from 19 institutions, representing multiple constituent databases. There are over 20 data-types within the Neotoma Paleoecological Database, including pollen microfossils, plant macrofossils, vertebrate fauna, diatoms, charcoal, biomarkers, ostracodes, physical sedimentology and water chemistry. Neotoma provides an underlying cyberinfrastructure that enables the development of common software tools for data ingest, discovery, display, analysis, and distribution, while giving domain scientists control over critical taxonomic and other data quality issues.",
-                record_count: 11955
-            },
-            {
-                title: 'earthchem',
-                website: 'http://ecl.earthchem.org/home.php',
-                image: 'earthchem.png',
-                description: "Community-driven preservation, discovery, access, and visualization of geochemical, geochronological, and petrological data.",
-                record_count: 639
-            },
-            {
-                title: 'xdomes',
-                website: 'http://xdomes.tamucc.edu',
-                image: 'xdomes.png',
-                description: "The 'Cross-Domain Observational Metadata for Environmental Sensing (X-DOMES)' is an NSF/EarthCube project tasked to develop prototypes for the application of standards-based description of environmental sensor metadata, including provenance, that is paramount to automated data discovery, access, archival, processing, as well as quality control and assessment.",
-                record_count: 17
-            },
-            {
-                title: 'National Ecological Observatory Network (NEON)',
-                website: 'http://www.neonscience.org',
-                image: 'neon.png',
-                description: "The National Ecological Observatory Network (NEON) is a continental-scale observatory designed to gather and provide 30 years of ecological data on the impacts of climate change, land use change and invasive species on natural resources and biodiversity. NEON is a project of the National Science Foundation, with many other U.S. agencies and NGOs cooperating.",
-                record_count: 179
-            },
-            {
-                title: 'Resource Registry',
-                website: 'http://www.earthcube.org/resourceregistry',
-                image: 'resource_registry.png',
-                description: "The EarthCube Resource Registry (ECRR) is intended to provide immediate access to a list of EC capabilities to understand what EC is, and what it isn’t. To support this goal, the ECRR project has developed several persistent resources available for wider EarthCube use.",
-                record_count: 274
-            }
-        ]
+        info: null
     }
   },
+    mounted () {
+    axios
+      .get('https://oss.geocodes-aws.earthcube.org/earthcube/reports/all/latest/report_stats.json')
+      .then(response => (
+          this.info = response.data))
+  },
+    methods: {
+    // try to avoid jquery hacks.
+      // do as a bootstrap modal. https://bootstrap-vue.org/docs/components/modal
+        showModal(t) {
+            console.log(t)
+            $('#'+t).modal('show')
+        }
+    }
 }
+
 </script>
 
 <style scoped lang="scss">
@@ -257,6 +164,28 @@ export default {
     &.not-collapsed .when_open {
         display: none;
     }
+}
+
+.customDropdown {
+    background-color: white;
+}
+
+.button-file {
+    border:0;
+    background:transparent;
+    color: gray;
+}
+
+.customTable {
+    background: #F8F9F9;
+}
+
+th {
+  display: table-cell;
+  vertical-align: inherit;
+  text-align: center;
+    background: #18598b;
+    color: white;
 }
 
 </style>
---
+++ client/src/components/help/report.vue
@@ -0,0 +1,330 @@
+<template>
+    <b-container fluid="md" class="mt-5">
+        <div>
+            <h2>Source: {{ this.source}}</h2>
+          <div  v-if="missing">
+
+            <b-card header="Missing Report">
+          <div  v-if="missing">
+            <b-link :href="missing_url" class="card-link">Download Original Report (JSON)</b-link>
+
+              <p>Report Date: {{ missing.date }}</p>
+              <ul>
+                  <li>Sitemap count: {{ missing.sitemap_count }}</li>
+                  <li>Summoned count: {{ missing.summoned_count }}</li>
+                  <li>Missing summoned count: {{ missing.missing_sitemap_summon_count }}</li>
+                  <li>Graph URN count: {{ missing.graph_urn_count }}</li>
+                  <li>Missing graph URN count: {{ missing.missing_summon_graph_count }}</li>
+              </ul>
+            </div>
+            </b-card>
+          <div  v-if="graphinfo">
+              <b-card header="Graph Stats">
+                <b-link :href="graph_url" class="card-link">Download Original Report (JSON )</b-link>
+              <p>Report Date: {{ graphinfo.date }}</p>
+              <ul>
+                  <b-list-group>
+                <b-list-group-item
+                  v-for="(item, index) in graphinfo.reports"
+                  :key="index"
+                  class = "customList"
+                >
+                    <li v-if="item.report=='dataset_count'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.datasetcount }}
+                        </p>
+                    </li>
+                  <li v-if="item.report=='types_count'">{{ item.report }} :
+                    <table class="customTable">
+                      <thead>
+                      <tr>
+                        <th>type</th>
+                        <th>scount</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr
+                          v-for="(report, report_index) in item.data"
+                          :key="report_index"
+                      >
+                        <td>{{ report.type }}</td>
+                        <td>{{ report.scount }}</td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </li>
+                  <li v-if="item.report=='type_count'">{{ item.report }} :
+                    <table class="customTable">
+                      <thead>
+                      <tr>
+                        <th>type</th>
+                        <th>scount</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr
+                          v-for="(report, report_index) in item.data"
+                          :key="report_index"
+                      >
+                        <td>{{ report.type }}</td>
+                        <td>{{ report.scount }}</td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </li>
+                    <li v-if="item.report=='triple_count'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.tripelcount }}
+                        </p>
+                    </li>
+                    <li v-if="item.report=='repos_with_keywords'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.kwcount }}
+                        </p>
+                    </li>
+                  <li v-if="item.report=='kw_count'">{{ item.report }} :
+                    <table class="customTable">
+                      <thead>
+                      <tr>
+                        <th>keyword</th>
+                        <th>counts</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr
+                          v-for="(report, report_index) in item.data"
+                          :key="report_index"
+                      >
+                        <td>{{ report.keyword }}</td>
+                        <td>{{ report.scount }}</td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </li>
+                    <li v-if="item.report=='graph_count_by_repo'">{{ item.report }} :
+                        <table class="customTable">
+                            <thead>
+                            <tr>
+                                <th>graphs</th>
+                                <th>datasets</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
+                              v-for="(report, report_index) in item.data"
+                              :key="report_index"
+                            >
+                                <td>{{ report.graphs }}</td>
+                                <td>{{ report.datasets }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                    <li v-if="item.report=='types_count'">{{ item.report }} :
+                        <table class="customTable">
+                            <thead>
+                            <tr>
+                                <th>type</th>
+                                <th>scount</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
+                              v-for="(report, report_index) in item.data"
+                              :key="report_index"
+                            >
+                                <td>{{ report.type }}</td>
+                                <td>{{ report.scount }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                    <li v-if="item.report=='version_count'">{{ item.report }} :
+                        <b-list-group>
+                            <b-list-group-item
+                              v-for="(report, report_index) in item.data"
+                              :key="report_index"
+                            >
+                                {{ report.versioncount }}
+                            </b-list-group-item>
+                        </b-list-group>
+                    </li>
+                    <li v-if="item.report=='variablename_count'">{{ item.report }} :
+                        <b-list-group>
+                            <b-list-group-item
+                              v-for="(report, report_index) in item.data"
+                              :key="report_index"
+                            >
+                                {{ report.variablenamecount }}
+                            </b-list-group-item>
+                        </b-list-group>
+                    </li>
+
+                    <li v-if="item.report=='types_count_by_repo'">{{ item.report }} :
+                        <table class="customTable">
+                            <thead>
+                            <tr>
+                                <th>type</th>
+                                <th>type_counts</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr
+                              v-for="(report, report_index) in item.data"
+                              :key="report_index"
+                            >
+                                <td>{{ report.type }}</td>
+                                <td>{{ report.type_counts }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </li>
+                    <li v-if="item.report=='mutilple_version_count_by_repo'">
+                        <p v-for="(report, report_index) in item.data" :key="report_index">
+                            {{ item.report }} : {{ report.versioncount }}
+                        </p>
+                    </li>
+
+                  <li v-if="item.report=='graph_sizes_count'">{{ item.report }} :
+                    <table class="customTable">
+                      <thead>
+                      <tr>
+                        <th>triples_per_jsonld</th>
+                        <th>count</th>
+                      </tr>
+                      </thead>
+                      <tbody>
+                      <tr
+                          v-for="(report, report_index) in item.data"
+                          :key="report_index"
+                      >
+                        <td>{{ report.triple_per_jsonld }}</td>
+                        <td>{{ report.count }}</td>
+                      </tr>
+                      </tbody>
+                    </table>
+                  </li>
+                </b-list-group-item>
+              </b-list-group>
+
+              </ul>
+              </b-card>
+            </div>
+          </div>
+        </div>
+    </b-container>
+</template>
+
+<script>
+
+import axios from "axios";
+import JSON5 from 'json5'
+
+import {mapState} from "vuex";
+
+
+export default {
+  name: "report.vue",
+  props: {
+    source: String,
+  },
+  data() {
+    return {
+        missing: null,
+        graphinfo: null,
+      missing_url: "",
+      graph_url: ""
+    }
+  },
+  computed: {
+    ...mapState(['FacetsConfig'])
+
+  },
+    mounted () {
+     // const source = this.$route.params.repo;
+      console.log(this.source);
+      const s3base = this.FacetsConfig.S3_REPORTS_URL;
+      this.missing_url = `${s3base}${ this.source}/latest/missing_report_graph.json`;
+      this.graph_url =`${s3base}${ this.source}/latest/graph_stats.json`;
+
+      this.fetchMissingReport();
+      this.fetchGraphStats();
+     }, methods: {
+      fetchMissingReport() {
+          axios
+              .get(this.missing_url)
+              .then(miss => (
+                  this.missing = miss.data))
+      },
+      fetchGraphStats() {
+
+          axios
+              .get(this.graph_url)
+              .then(response => {
+                    try{
+                      this.graphinfo = JSON5.parse(response.data );
+                      console.log(this.graphinfo);
+                    } catch (error ){
+                      this.graphinfo = response.data ;
+                      console.log(this.graphinfo);
+                    }
+
+                 }
+              )
+      }
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+@import '~/src/assets/bootstrapcss/custom';
+
+.logo {
+    max: {
+        width: 50%;
+    }
+    min: {
+        height: 100px;
+    }
+}
+
+.card-text {
+    &.collapsed .when_closed,
+    &.not-collapsed .when_open {
+        display: none;
+    }
+}
+
+.customDropdown {
+    background-color: white;
+}
+
+.button-file {
+    border:0;
+    background:transparent;
+    color: gray;
+}
+
+.customTable {
+    background: #F8F9F9;
+}
+
+th {
+  display: table-cell;
+  vertical-align: inherit;
+  text-align: center;
+    background: #18598b;
+    color: white;
+}
+
+th, td {
+  padding-left: 30px;
+  padding-right: 30px;
+}
+
+.customList {
+    border: none
+}
+
+</style>
