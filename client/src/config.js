f5221001367bd9bcb5b70382915013c03b38dc8f
David Valentine <david.valentine@gmail.com>
2024-01-29 12:24:47 -0800

    error on netlify

--- client/src/config.js
+++ client/src/config.js
@@ -20,7 +20,7 @@ const facetConfigFunction = () => {
 // we really cannot use a process.env value for file name, because that gets embedded in webpack,
 // aka would need a new container for every file
      //fetch(process.env.BASE_URL + "config/config.yaml") // vuec
-    const configfile = import.meta.env.FACETS_CONFIG_FILE | 'config/config.yaml'
+    const configfile = import.meta.env.VITE_FACETS_CONFIG_FILE ? import.meta.env.VITE_FACETS_CONFIG_FILE :  'config/config.yaml'
     const configurl= import.meta.env.BASE_URL + configfile
     fetch(configurl)//vite
     .then((response) => response.text())
