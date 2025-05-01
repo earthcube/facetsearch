f2819307ddd5c420292f5a7481e33a368f5eb7ce
Ya-Lan Yang <ylyang@illinois.edu>
2025-05-01 23:18:38 +0200

    removed unused codes

--- client/src/components/dataset/datasetLocation.vue
+++ client/src/components/dataset/datasetLocation.vue
@@ -3,30 +3,6 @@
     <b-card-title>Location</b-card-title>
 
     <div id="myMap" ref="myMap" style="width: 100%; height: 320px"></div>
-
-    <!--      <l-map ref="myMap" id="myMap" :zoom="zoom"-->
-    <!--             :center="center"-->
-    <!--             style="height: 400px; width:400px"-->
-    <!--      >-->
-    <!--        &lt;!&ndash;          <l-tile-layer&ndash;&gt;-->
-    <!--        &lt;!&ndash;              :url="mapboxurl"&ndash;&gt;-->
-    <!--        &lt;!&ndash;              :attribution="attribution"&ndash;&gt;-->
-
-    <!--        &lt;!&ndash;          />&ndash;&gt;-->
-    <!--        &lt;!&ndash;          <l-marker :lat-lng="withTooltip">&ndash;&gt;-->
-    <!--        &lt;!&ndash;            <l-tooltip :options="{ permanent: true, interactive: true }">&ndash;&gt;-->
-    <!--        &lt;!&ndash;              <div @click="innerClick">&ndash;&gt;-->
-    <!--        &lt;!&ndash;                I am a tooltip&ndash;&gt;-->
-    <!--        &lt;!&ndash;                <p v-show="showParagraph">&ndash;&gt;-->
-    <!--        &lt;!&ndash;                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque&ndash;&gt;-->
-    <!--        &lt;!&ndash;                  sed pretium nisl, ut sagittis sapien. Sed vel sollicitudin nisi.&ndash;&gt;-->
-    <!--        &lt;!&ndash;                  Donec finibus semper metus id malesuada.&ndash;&gt;-->
-    <!--        &lt;!&ndash;                </p>&ndash;&gt;-->
-    <!--        &lt;!&ndash;              </div>&ndash;&gt;-->
-    <!--        &lt;!&ndash;            </l-tooltip>&ndash;&gt;-->
-    <!--        &lt;!&ndash;          </l-marker>&ndash;&gt;-->
-
-    <!--      </l-map>-->
   </b-card>
 </template>
 
@@ -57,7 +33,6 @@ import {
   frameJsonLD,
 } from "@/api/jsonldObject";
 import { Icon } from "leaflet";
-//import jsonld from "jsonld";
 
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
@@ -97,19 +72,6 @@ export default {
   mounted() {
     this.hasSpatial = false;
     this.mapboxurl = `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${this.FacetsConfig.MAPBOX_API_KEY}`;
-    // this.$nextTick(() => {
-    //   //this.$refs.myMap.mapObject.setView(this.center, 13);
-    //   this.mymap = L.map(this.$refs.myMap.id).setView(this.center, 13);
-    //   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    //     maxZoom: 12,
-    //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-    //         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    //     id: 'mapbox/streets-v11',
-    //     tileSize: 512,
-    //     zoomOffset: -1
-    //   }).addTo(this.mymap);
-    //
-    // });
   },
   computed: {
     ...mapState(["jsonLdObj", "jsonLdCompact", "FacetsConfig"]),
@@ -127,19 +89,7 @@ export default {
     toMap: function () {
       var obj = this.jsonLdObj;
       var self = this;
-      // move all the caluations out of nextTick
-      //       let datasetFrame = JSON.parse(`
-      // {
-      //   "@context": {
-      //     "@vocab": "https://schema.org/",
-      //         "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-      //         "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
-      //         "schema": "https://schema.org/",
-      //         "xsd": "http://www.w3.org/2001/XMLSchema#"
-      //   },
-      //   "@type": "schema:Dataset"
-      // }`)
-      //       jsonld.frame(obj, datasetFrame).then(
+
       frameJsonLD(obj, "Dataset").then((obj) => {
         let name = schemaItem("name", obj);
         let s_spatialCoverage = schemaItem("spatialCoverage", obj);
@@ -157,46 +107,10 @@ export default {
           let line = getFirstGeoShape(s_spatialCoverage, "line");
           let points = getGeoCoordinates(s_spatialCoverage);
 
-          //this.s_identifier_doi= ""
-
-          // let detail = {
-          //   name: this.name,
-          //   points: points,
-          //   poly: poly,
-          //   box: box,
-          //   placename: placename
-          // }
-
-          // this.$refs.myMap.mapObject.ANY_LEAFLET_MAP_METHOD();
           self.center = [46.8832566, -114.0870563]; // original centerpoint hell, montanta
           if (!(box || points || poly || line)) {
             return;
           }
-          // this needs to be cleaned up, and probably set a
-          // standard geomentry objects and simplify the adding
-          // of the objects to leaflet.
-          // if (points && points.length > 0) {
-          //
-          //   self.center = points[0] // first one
-          //   console.log(`firstpoint will be center ${self.center}`)
-          // } else if (box) {
-          //   // calc centerpoint of box
-          //   //var points = e.detail.box.split(" ")
-          //
-          //   var northing = (box[0][0] + box[1][0]) / 2
-          //   var easting = (box[0][1] + box[1][1]) / 2
-          //   console.log(`box center: ${northing} ${easting}`)
-          //   self.center = [northing, easting]
-          // } else if (line) {
-          //   self.center(L.polyline(line).getCenter())
-          //   // do polygon here
-          // } else if (poly){
-          //   self.center(L.polygon(poly).getCenter())
-          // } else {
-          //   // can do anything, so let's turn off
-          //   console.log("unrecognized spatial object. Disabling location")
-          //   this.hasSpatial = false
-          // }
 
           // move all the caluations out of nextTick
           this.$nextTick(() => {
@@ -212,8 +126,6 @@ export default {
               tileSize: 512,
               zoomOffset: -1,
             }).addTo(self.mymap);
-            // reactive... set
-            // var mymap = L.map('mapid').setView(centerpoint, 13);
 
             if (points && points.length > 0) {
               self.center = points[0]; // first one
@@ -263,7 +175,6 @@ export default {
             } else {
               L.marker(self.center).addTo(self.mymap);
             }
-            // v-show... causing issues, solution https://stackoverflow.com/questions/36246815/data-toggle-tab-does-not-download-leaflet-map/36257493#36257493
             setTimeout(() => {
               this.mymap.panTo(self.center);
               this.mymap.invalidateSize();
