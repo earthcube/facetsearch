840546ab724f22bb69fa76e8c2dfc36aaffa21b3
Ya-Lan Yang <ylyang@illinois.edu>
2026-02-12 04:10:46 -0600

    fix keywords, publisher

--- client/src/components/facetsearch/FacetText.vue
+++ client/src/components/facetsearch/FacetText.vue
@@ -2,18 +2,8 @@
   <div class="filter_card">
     <b-button v-b-toggle="'accordion_text_' + facetSetting.field" block squared>
       {{ facetSetting.title }}
-      <b-icon
-        icon="square"
-        class="when-open"
-        scale="0.8"
-        aria-hidden="true"
-      ></b-icon>
-      <b-icon
-        icon="plus-square"
-        class="when-closed"
-        scale="0.8"
-        aria-hidden="true"
-      ></b-icon>
+      <b-icon icon="square" class="when-open" scale="0.8" aria-hidden="true" />
+      <b-icon icon="plus-square" class="when-closed" scale="0.8" aria-hidden="true" />
     </b-button>
 
     <b-collapse
@@ -21,253 +11,76 @@
       :visible="facetSetting.open"
     >
       <b-list-group flush>
-        <FacetTextItem
+        <!-- Wrap each item so we can reliably catch click in Vue 3 -->
+        <b-list-group-item
           v-for="(info, term) in facetItems"
-          :id="facetStore[facetSetting.field][term].id"
           :key="info.id"
-          :term="term"
-          :count="info.count"
-          :facet-setting="facetSetting"
-          :is-active="info.isActive"
-          :fieldname="fieldname"
-        ></FacetTextItem>
+          button
+          class="facet-row d-flex align-items-center justify-content-between"
+          @click="onToggle(term, !info.isActive)"
+        >
+          <FacetTextItem
+            :id="info.id"
+            :term="term"
+            :count="info.count"
+            :facet-setting="facetSetting"
+            :is-active="info.isActive"
+            :fieldname="fieldname"
+            class="w-100"
+          />
+        </b-list-group-item>
+
       </b-list-group>
     </b-collapse>
   </div>
 </template>
 
 <script>
-//import Vue from 'vue'
 import { event as gtagevent } from "vue-gtag";
-import _ from "underscore";
 import FacetTextItem from "@/components/facetsearch/FacetTextItem.vue";
-//import {bus} from "../../main.js"
-//import { inject } from 'vue'
+
 export default {
   name: "FacetText",
   components: { FacetTextItem },
-  inject: ["toggleFilter", "filtersState"],
-  // setup(){
-  //   // eslint-disable-next-line
-  //   const toggleFilter = inject("toggleFilter")
-  //   // eslint-disable-next-line
-  //   const facetStore = inject('facetStore')
-  // },
+
+  inject: ["toggleFilter"],
+
   props: {
-    facetSetting: Object,
-    facetStore: Object,
+    facetSetting: { type: Object, required: true },
+    facetStore: { type: Object, required: true },
     fieldname: String,
-    //   "state":Object,
-    //   "currentResults": Array
-  },
-  data() {
-    return {
-      // watch facetStore
-      facetItems: this.facetStore[this.facetSetting.field],
-    };
   },
 
   computed: {
-    // facetList: function( ){
-    //   var self = this;
-    //   var count = 0 ;
-    //   var facetSetting = this.facetSetting;
-    //   // Update the count for each facet and item:
-    //   // intialize the count to be zero
-    //   //this.resetFacetCount(settings);
-    //   // then reduce the items to get the current count for each facet
-    //  // _.each(settings.facets, function (facet) {
-    //  var facetItems =  _.each(self.currentResults, function (item) {
-    //       if (_.isArray(item[facetSetting.field])) {
-    //         _.each(item[facetSetting.field], function (facetitem) {
-    //           if (!_.isEmpty(facetitem)) {
-    //             count += 1;
-    //           }
-    //
-    //         });
-    //       } else {
-    //         if (item[facetSetting.field] !== undefined) {
-    //           if (!_.isEmpty(item[facetSetting.field])) {
-    //             count += 1;
-    //           }
-    //
-    //         }
-    //       }
-    //     });
-    //  // });
-    //
-    //   // remove confusing 0 from facets where a filter has been set
-    //
-    //       if (count == 0 && self.settings.state.filters[facetSetting.field].length) count = "+";
-    //
-    //
-    //
-    //   //return settings.facetStore[facetSetting.field][item[facetSetting]].count;
-    //   return facetItems;
-    // }
-  },
-  mounted() {
-    /**** vue3 off **/
-    // var self = this;
-    //   bus.$on('facetupdate', () => {
-    //     console.log("facetupdate event");
-    //     self.facetItems = self.facetStore[self.facetSetting.field];
-    //     self.updateFacetItems()
-    //   })
-  },
-  methods: {
-    updateFacetItems: function () {
-      console.log("facetupdateitems methtod called event");
-      this.$forceUpdate();
-      //  this.facetItems = this.facetStore[this.facetSetting.field];
+    // Reactive view into facetStore[field]
+    facetItems() {
+      const field = this.facetSetting?.field;
+      return field && this.facetStore && this.facetStore[field]
+        ? this.facetStore[field]
+        : {};
     },
-    _handleClick: function (event) {
-      const self = this;
-      console.log(event);
-      //var filter = this.getFilterById(this.id);
-      var filter = self.getFilterById(event.target.id);
-      //var filter = this.facetSetting
-      // use $nextTick to delay an processing until after the entire dom has been updated.
-      // otherwise we get a an error about a null key
-      //self.$nextTick(() =>  self.toggleFilter(filter.facetname, filter.filtername) )
-      //  self.toggleFilter(filter.field, filter.title); //vue2
-      this.toggleFilter(filter.field, filter.title); // vue3
+  },
 
-      // $(this.facetSelector).trigger("facetedsearchfacetclick", filter);
-      /**** vue3 off **/
-      // bus.$emit("facetedsearchfacetclick", filter)
+  methods: {
+    onToggle(term, nextActive) {
+      const field = this.facetSetting.field;
 
-      //   Vue.$gtag.event('select_content', {
+      console.log("FacetText onToggle:", { field, term, nextActive });
+
+      // IMPORTANT: pass boolean 3rd arg
+      this.toggleFilter(field, term, !!nextActive);
+
+      // analytics (optional)
       gtagevent("select_content", {
-        content_type: filter.field,
-        item_id: filter.title,
+        content_type: field,
+        item_id: term,
       });
-      //Vue.$gtag.event('select_facet', {
       gtagevent("select_facet", {
-        // content_type:filter.field,
-        // item_id: filter.title,
         event_category: "engagement",
         event_label: "facet_clicked",
-        value: `${filter.field}:${filter.title}`,
+        value: `${field}:${term}`,
       });
-      //order();
-      // updateFacetUI();
-      // updateResults();
     },
-
-    /**
-     * get a facetname and filtername by the unique id that is created in the beginning
-     */
-    getFilterById: function (id) {
-      var result = false;
-      _.each(this.facetStore, function (facet, facetname) {
-        _.each(facet, function (filter, filtername) {
-          if (filter.id == id) {
-            result = { field: facetname, title: filtername };
-          }
-        });
-      });
-      return result;
-    },
-    //   // toggleFilter: function(key, value)
-    //   // {
-    //   //   this.state.filters[key] = this.state.filters[key] || [];
-    //   //   if (_.indexOf(this.state.filters[key], value) == -1) {
-    //   //     this.state.filters[key].push(value);
-    //   //     //this.facetStore[key][value].isActive = true;
-    //   //    Vue.set( this.facetStore[key][value], 'isActive',true);
-    //   //    // this.activeFacet=true;
-    //   //   } else {
-    //   //     this.state.filters[key] = _.without(this.state.filters[key], value);
-    //   //     if (this.state.filters[key].length == 0) {
-    //   //       delete this.state.filters[key];
-    //   //       //this.facetStore[key][value].isActive = false;
-    //   //
-    //   //       Vue.set(this.facetStore[key][value], 'isActive',false);
-    //   //     //  this.activeFacet=false;
-    //   //     }
-    //   //   }
-    //   //   this.$parent.$parent.filter();
-    //   // },
-    //   // facetCount: function( ){
-    //   //   var count = 0 ;
-    //   //   var facetSetting = this.facetSetting;
-    //   //   // Update the count for each facet and item:
-    //   //   // intialize the count to be zero
-    //   //   //this.resetFacetCount(settings);
-    //   //   // then reduce the items to get the current count for each facet
-    //   //   // _.each(settings.facets, function (facet) {
-    //   //   _.each(this.currentResults, function (item) {
-    //   //     if (_.isArray(item[facetSetting.field])) {
-    //   //       _.each(item[facetSetting.field], function (facetitem) {
-    //   //         if (_.isEmpty(facetitem)) {
-    //   //           return;
-    //   //         }
-    //   //         count += 1;
-    //   //       });
-    //   //     } else {
-    //   //       if (item[facetSetting.field] !== undefined) {
-    //   //         if (_.isEmpty(item[facetSetting.field])) {
-    //   //           return;
-    //   //         }
-    //   //         count += 1;
-    //   //       }
-    //   //     }
-    //   //   });
-    //   //   // });
-    //   //
-    //   //   // remove confusing 0 from facets where a filter has been set
-    //   //
-    //   //   if (count == 0 && this.settings.state.filters[facetSetting.field].length) count = "+";
-    //   //
-    //   //
-    //   //
-    //   //   //return settings.facetStore[facetSetting.field][item[facetSetting]].count;
-    //   //   return count;
-    //   // },
-    //   /**
-    //    * resets the facet count
-    //    */
-    //   resetFacetCount: function (settings) {
-    //     _.each(settings.facetStore, function (items, facetname) {
-    //       _.each(items, function (value, itemname) {
-    //         settings.facetStore[facetname][itemname].count = 0;
-    //         settings.facetStore[facetname][itemname].isActive = false;
-    //       });
-    //     });
-    //   },
-    //   setFacetCount: function(settings){
-    //     // Update the count for each facet and item:
-    //     // intialize the count to be zero
-    //     this.resetFacetCount(settings);
-    //     // then reduce the items to get the current count for each facet
-    //     _.each(settings.facets, function (facet) {
-    //       _.each(settings.currentResults, function (item) {
-    //         if (_.isArray(item[facet.field])) {
-    //           _.each(item[facet.field], function (facetitem) {
-    //             if (_.isEmpty(facetitem)) {
-    //               return;
-    //             }
-    //             settings.facetStore[facet.field][facetitem].count += 1;
-    //           });
-    //         } else {
-    //           if (item[facet] !== undefined) {
-    //             if (_.isEmpty(item[facet.field])) {
-    //               return;
-    //             }
-    //             settings.facetStore[facet.field][item[facet.field]].count += 1;
-    //           }
-    //         }
-    //       });
-    //     });
-    //
-    //     // remove confusing 0 from facets where a filter has been set
-    //     _.each(settings.state.filters, function (filters, facettitle) {
-    //       _.each(settings.facetStore[facettitle], function (facet) {
-    //         if (facet.count === 0 && settings.state.filters[facettitle].length) facet.count = "+";
-    //       });
-    //     });
-    //   }
   },
 };
 </script>
@@ -303,6 +116,20 @@ export default {
     border: 0px;
   }
 
+  .facet-row {
+      padding: 0.1rem 0.1rem !important;   // smaller padding
+      min-height: unset !important;
+      line-height: 0.5;
+      border: 0 !important;
+      border-bottom: 0 !important;
+      border-radius: 0 !important;
+    }
+
+    // If bootstrap adds extra focus/outline spacing
+    .facet-row:focus {
+      box-shadow: none !important;
+    }
+
   .list-group {
     overflow: {
       y: auto;
@@ -314,7 +141,6 @@ export default {
   }
 }
 
-//make flat color
 .btn-secondary,
 .btn-secondary:hover {
   background: {
--- client/src/components/facetsearch/Search.vue
+++ client/src/components/facetsearch/Search.vue
@@ -1,4 +1,3 @@
-
 <template>
   <b-container fluid="md" class="mt-3">
     <b-overlay
@@ -36,20 +35,16 @@
 <script>
 import Results from "@/components/facetsearch/Results.vue";
 import Facets from "@/components/facetsearch/Facets.vue";
-import _ from "underscore";
 import ResultHeader from "@/components/facetsearch/ResultHeader.vue";
-import {
-  mapActions,
-  mapGetters,
-  mapState,
-  mapMutations,
-} from "vuex";
+import { mapActions, mapGetters, mapState, mapMutations } from "vuex";
 import feedback from "@/components/feedback/feedback.vue";
 import { v5 as uuidv5 } from "uuid";
+import * as _ from "underscore";
 
 export default {
   name: "Search",
-  provide: function () {
+
+  provide() {
     return {
       toggleFilter: this.toggleFilter,
       esTemplateOptions: this.esTemplateOptions,
@@ -62,6 +57,7 @@ export default {
       filter: this.filter,
     };
   },
+
   computed: {
     ...mapState([
       "results",
@@ -73,6 +69,7 @@ export default {
     ]),
     ...mapGetters(["hasMicroCache", "getMicroCache"]),
   },
+
   watch: {
     results: "search",
     textQuery: "newTextSearch",
@@ -85,7 +82,7 @@ export default {
     title: String,
     textQuery: String,
     resourceType: String,
-    exact: String
+    exact: String,
   },
 
   components: {
@@ -116,25 +113,30 @@ export default {
       lastError: undefined,
     };
   },
+
   created() {
     this.n = this.FacetsConfig.LIMIT_DEFAULT;
     this.facets = this.FacetsConfig.FACETS;
+
     const facetsList = {};
-    _.each(this.facets, function (facet) {
+    _.each(this.facets, (facet) => {
       facetsList[facet.field] = {};
     });
     this.facetStore = facetsList;
   },
+
   mounted() {
     console.log(window.location.href);
     this.o = 0;
     this.queryRunning = true;
+
     this.setTextQuery(this.textQuery);
     this.setResourceTypeQuery("all");
     this.setSearchExactMatch(this.exact);
-    
+
     let paramString = window.location.href.split("?")[1];
     let queryString = new URLSearchParams(paramString);
+
     for (let pair of queryString.entries()) {
       if (pair[0] === "q") continue;
       if (pair[0] === "resourceType" && pair[1] == "all") continue;
@@ -143,23 +145,23 @@ export default {
     }
 
     let queryObj = this.getQueryObj();
-
     if (this.hasMicroCache(queryObj.uuid)) {
       console.log("lru has " + queryObj.uuid);
       this.search();
     }
   },
+
   methods: {
     ...mapMutations(["setTextQuery", "setResourceTypeQuery", "setSearchExactMatch"]),
     ...mapActions(["getResults", "getQueryTemplate", "addtoMicroCache"]),
-    
+
     updateYearRange(start, end) {
       this.$set(this.filtersState.filters, "startYear", [start]);
       this.$set(this.filtersState.filters, "endYear", [end]);
       this.filter();
     },
-    
-    getQueryObj: function () {
+
+    getQueryObj() {
       let activeFilters = [];
       let queryObj = {
         textQuery: this.textQuery,
@@ -173,10 +175,11 @@ export default {
       let uuid = uuidv5(string2uuid, uuidv5.URL);
       return { uuid: uuid, query: queryObj };
     },
-    
-    newTextSearch: function () {
+
+    newTextSearch() {
       this.queryRunning = true;
       this.setTextQuery(this.textQuery);
+
       this.getResults(this.getQueryObj())
         .then(() => {
           this.queryRunning = false;
@@ -192,122 +195,160 @@ export default {
         });
     },
 
-    search: function () {
+    // Normalize fields so facet filtering works:
+    // - kw arrives as "a, b, c" (GROUP_CONCAT) => convert to ["a","b","c"]
+    // - pubname trim
+    normalizeResults(results) {
+      return (results || []).map((it) => {
+        const out = { ...it };
+
+        // keywords facet expects array intersection to work
+        if (typeof out.kw === "string") {
+          out.kw = out.kw
+            .split(",")
+            .map((s) => s.trim())
+            .filter(Boolean);
+        }
+
+        // ensure pubname is stable (trim)
+        if (typeof out.pubname === "string") {
+          out.pubname = out.pubname.trim();
+        }
+
+        // optional: normalize "No Publisher" spacing
+        if (!out.pubname || out.pubname === "") out.pubname = "No Publisher";
+
+        return out;
+      });
+    },
+
+    search() {
       let queryObj = this.getQueryObj();
       this.feedBackItemId = "search?q=" + queryObj.query?.textQuery;
       this.queryRunning = false;
-      
-      // Use Vuex results directly - this is the key change
-      this.items = this.results;
-      
+
+      // Vuex results -> normalize -> local items
+      this.items = this.normalizeResults(this.results);
+
       this.initFacetCounts();
       this.filter();
     },
-    
+
     setResultLimit(n) {
       this.n = n;
     },
-    
+
     setSearchExactmatch(b) {
       this.searchExactMatch = b;
     },
 
-    initFacetCounts: function () {
+    initFacetCounts() {
       let items = this.items;
       let facets = this.facets;
       let facetStore = this.facetStore;
       let facetSortOption = this.facetSortOption;
 
-      _.each(items, function (item) {
-        _.each(facets, function (facet) {
-          if (_.isArray(item[facet.field])) {
-            _.each(item[facet.field], function (facetitem) {
-              if (_.isEmpty(facetitem)) {
-                return;
-              }
-              facetStore[facet.field][facetitem] = facetStore[facet.field][
-                facetitem
-              ] || {
-                count: 0,
-                id: _.uniqueId("facet_"),
-                isActive: false,
-              };
+      _.each(items, (item) => {
+        _.each(facets, (facet) => {
+          if (Array.isArray(item[facet.field])) {
+            _.each(item[facet.field], (facetitem) => {
+              if (!facetitem || facetitem === "") return;
+              facetStore[facet.field][facetitem] =
+                facetStore[facet.field][facetitem] || {
+                  count: 0,
+                  id: _.uniqueId("facet_"),
+                  isActive: false,
+                };
             });
           } else {
             if (item[facet.field] !== undefined) {
-              if (_.isEmpty(item[facet.field])) {
-                return;
-              }
-              facetStore[facet.field][item[facet.field]] = facetStore[
-                facet.field
-              ][item[facet.field]] || {
-                count: 0,
-                id: _.uniqueId("facet_"),
-                isActive: false,
-              };
+              if (!item[facet.field] || item[facet.field] === "") return;
+              facetStore[facet.field][item[facet.field]] =
+                facetStore[facet.field][item[facet.field]] || {
+                  count: 0,
+                  id: _.uniqueId("facet_"),
+                  isActive: false,
+                };
             }
           }
         });
       });
-      
+
       // Sort facets
-      _.each(facetStore, function (facetData, facettitle) {
-        var sorted = _.keys(facetStore[facettitle]).sort();
+      _.each(facetStore, (facetData, facettitle) => {
+        let sorted = _.keys(facetStore[facettitle]).sort();
         if (facetSortOption && facetSortOption[facettitle]) {
           sorted = _.union(facetSortOption[facettitle], sorted);
         }
-        var sortedstore = {};
-        _.each(sorted, function (el) {
+        let sortedstore = {};
+        _.each(sorted, (el) => {
           sortedstore[el] = facetStore[facettitle][el];
         });
         facetStore[facettitle] = sortedstore;
       });
     },
 
-    filter: function () {
+    filter() {
       const self = this;
       let facets = this.facets;
       let activeFilters = this.filtersState.filters;
-      
-      // Start with items from Vuex store (which may already be filtered by depth)
-      self.currentResults = this.results;
 
-      // Filter by active facet filters
-      _.each(activeFilters, function (filtervalues, filter) {
-        if (_.isEmpty(filtervalues)) {
-          return;
-        }
-        
-        self.currentResults = _.filter(self.currentResults, function (item) {
-          if (_.isArray(item[filter])) {
-            return _.intersection(filtervalues, item[filter]).length > 0;
-          } else {
-            return _.contains(filtervalues, item[filter]);
+      console.log("filter() called");
+      console.log("Active filters:", JSON.stringify(activeFilters));
+
+      // ALWAYS filter from normalized items
+      let baseResults = this.items;
+      console.log("Base results count:", baseResults.length);
+
+      self.currentResults = [...baseResults];
+
+      _.each(activeFilters, (filtervalues, filterKey) => {
+        console.log("Applying filter:", filterKey, "with values:", filtervalues);
+
+        if (!filtervalues || filtervalues.length === 0) return;
+
+        const beforeFilterCount = self.currentResults.length;
+
+        self.currentResults = _.filter(self.currentResults, (item) => {
+          const v = item[filterKey];
+
+          if (Array.isArray(v)) {
+            // array facet (kw after normalization)
+            const intersect = _.intersection(filtervalues, v);
+            return intersect.length > 0;
           }
+
+          // scalar facet
+          return _.contains(filtervalues, v);
         });
+
+        console.log(
+          `Filter '${filterKey}' reduced results from ${beforeFilterCount} to ${self.currentResults.length}`
+        );
       });
 
-      // Update facet counts based on filtered results
-      _.each(facets, function (facet) {
-        _.each(self.facetStore[facet.field], function (facetData, facetitem) {
+      console.log("Final current results count:", self.currentResults.length);
+
+      // reset facet counts
+      _.each(facets, (facet) => {
+        _.each(self.facetStore[facet.field], (facetData) => {
           facetData.count = 0;
         });
       });
 
-      _.each(self.currentResults, function (item) {
-        _.each(facets, function (facet) {
-          if (_.isArray(item[facet.field])) {
-            _.each(item[facet.field], function (facetitem) {
+      // recount based on filtered results
+      _.each(self.currentResults, (item) => {
+        _.each(facets, (facet) => {
+          const v = item[facet.field];
+          if (Array.isArray(v)) {
+            _.each(v, (facetitem) => {
               if (self.facetStore[facet.field][facetitem]) {
                 self.facetStore[facet.field][facetitem].count++;
               }
             });
           } else {
-            if (
-              item[facet.field] &&
-              self.facetStore[facet.field][item[facet.field]]
-            ) {
-              self.facetStore[facet.field][item[facet.field]].count++;
+            if (v && self.facetStore[facet.field][v]) {
+              self.facetStore[facet.field][v].count++;
             }
           }
         });
@@ -315,29 +356,27 @@ export default {
 
       // Sort results
       if (self.filtersState.orderBy === "name") {
-        self.currentResults = _.sortBy(self.currentResults, function (item) {
-          return item.name;
-        });
+        self.currentResults = _.sortBy(self.currentResults, (item) => item.name);
       } else if (self.filtersState.orderBy === "pubname") {
-        self.currentResults = _.sortBy(self.currentResults, function (item) {
-          return item.publisher;
-        });
+        // FIX: use pubname (your data field), not publisher
+        self.currentResults = _.sortBy(self.currentResults, (item) => item.pubname);
       } else if (self.filtersState.orderBy === "date") {
-        self.currentResults = _.sortBy(self.currentResults, function (item) {
-          return item.datePublished;
-        });
+        self.currentResults = _.sortBy(self.currentResults, (item) => item.datePublished);
       }
     },
 
-    order: function (orderBy) {
+    order(orderBy) {
       this.filtersState.orderBy = orderBy;
       this.filter();
     },
 
-    toggleFilter: function (facetKey, facetValue, isActive) {
+    toggleFilter(facetKey, facetValue, isActive) {
+      console.log("toggleFilter called:", { facetKey, facetValue, isActive });
+
       if (isActive) {
-        this.filtersState.filters[facetKey] =
-          this.filtersState.filters[facetKey] || [];
+        if (!this.filtersState.filters[facetKey]) {
+          this.$set(this.filtersState.filters, facetKey, []);
+        }
         this.filtersState.filters[facetKey].push(facetValue);
       } else {
         this.filtersState.filters[facetKey] = _.without(
@@ -345,30 +384,34 @@ export default {
           facetValue
         );
       }
-      
+
+      console.log("After - filtersState.filters:", JSON.stringify(this.filtersState.filters));
+
       if (this.facetStore[facetKey] && this.facetStore[facetKey][facetValue]) {
         this.facetStore[facetKey][facetValue].isActive = isActive;
       }
-      
+
       this.filter();
     },
 
-    clearFilters: function () {
+    clearFilters() {
       const self = this;
-      _.each(this.filtersState.filters, function (filtervalues, filter) {
-        _.each(filtervalues, function (value) {
-          if (self.facetStore[filter] && self.facetStore[filter][value]) {
-            self.facetStore[filter][value].isActive = false;
+
+      _.each(this.filtersState.filters, (filtervalues, filterKey) => {
+        _.each(filtervalues, (value) => {
+          if (self.facetStore[filterKey] && self.facetStore[filterKey][value]) {
+            self.facetStore[filterKey][value].isActive = false;
           }
         });
       });
+
       this.filtersState.filters = {};
-      
-      // Also reset depth filter in Vuex
+
+      // Also reset depth filter in Vuex (keeping your existing behavior)
       if (this.$store.state.allResults && this.$store.state.allResults.length > 0) {
-        this.$store.commit('resetFilters');
+        this.$store.commit("resetFilters");
       }
-      
+
       this.filter();
     },
   },
